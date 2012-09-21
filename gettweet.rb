ce13c523f8a3c4fa8521a0a96dc7f2b5bff2004a
kimihito <tatsurotamashiro@gmail.com>
2012-09-22 03:15:25 +0900

    index.erb

--- gettweet.rb
+++ gettweet.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 #-*- coding: utf-8 -*-
 require "twitter"
+
 Twitter.configure do |config|
   config.consumer_key = '2KMstxQcaqgMRKuPWtiLw'
   config.consumer_secret = 'Z6nRZHudkuEapsaJNfANko6oUpUJSZ3AQCAfpXwDf6s'
@@ -8,16 +9,55 @@ Twitter.configure do |config|
   config.oauth_token_secret = 'C7iyT5eR5TQSZbGC1QdO6oBMCTw1Jb0u67WJMIy3xmc'
 end
 
+def hashtag_tweet(hashtag)
+  Twitter.search("#"+hashtag+" -rt",:rpp => 100,:include_entities => 1).results
+end
+
+def photowish_tweets
+  hashtag_tweet("photowish").select{|tweet| expanded_url?(tweet)}
+end
+
+def expanded_url(tweet)
+  tweet.urls.select{|u| u.expanded_url}.map{|u| u.expanded_url}
+end
+
+def expanded_url?(tweet)
+  #ツイートに添付されたすべてのURLの中に,expanded_urlが1つでも含まれていればtrue
+  expanded_url(tweet).count != 0
+end
+
+
+
+
+
 #TODO 画像にアクセスするURLの関数を作る
-  def getimg(hashtag)
-    imgurl = ''
-    imgurl = Twitter.search("#"+hashtag+" -rt",:rpp => 1,:include_entities => 1).results.first.urls[0].expanded_url
-
-    if /twitpic/ =~ imgurl
-      url = imgurl.split(/\//)[-1]
-      image = 'http://twitpic.com/show/large/' + url
+def getimg(hashtag)
+  imgurl = ''
+  images = []
+  texts = []
+  Twitter.search("#"+hashtag+" -rt",:rpp => 100,:include_entities => 1).results.each do |result|
+    begin
+      texts << result.text
+      imgurl = isexpanded(result)
+      if /twitpic/ =~ imgurl
+        url = imgurl.split(/\//)[-1]
+        images << 'http://twitpic.com/show/large/' + url
+      end
+    rescue
     end
-    #TODO 他の画像投稿サービスを使ってimgを取得する。  
-
-    return image
   end
+  images
+end
+
+#expanded_urlがあるかどうかを確認する関数
+def isexpanded(result)
+  if result.urls[0].expanded_url == nil
+    nil
+  else
+    result.urls[0].expanded_url
+  end
+end
+
+#TODO 他の画像投稿サービスを使ってimgを取得する。  
+puts getimg("photowish")
+
--- home.rb
+++ home.rb
@@ -2,15 +2,36 @@
 #-*- coding: utf-8 -*-
 require 'rubygems'
 require 'sinatra'
+require './gettweet'
 
 get '/' do
-  "Hello World"
+  @tweets = photowish_tweets
+  erb :index
 end
 
-get '/hello/:name' do
-  "Hello there,#{params[:name]}."
+helpers do
+  def instagram(url)
+    #do something
+  end
+
+  def twipic(url)
+    imgurl = ''
+    url = url.split(/\//)[-1]
+    imgurl = 'http://twitpic.com/show/large/' + url
+  end
+
+  def image_url(tweet)
+    url = expanded_url(tweet).first
+    case url
+    when /http:\/\/twitpic/
+      twipic(url)
+    when /http:\/\/instagr.am/
+      instagram(url)
+    else
+      url #none
+    end
+  end
 end
 
-get '/test' do
-  "Shotgun test"
-end
+
+
